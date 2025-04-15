fa6ea067a58021a33c41461ab14f1fc17cbbc797
ArmaanGilani <armangilani777@gmail.com>
2025-04-15 23:33:48 +0530

    Initial commit for WebApplication1 .NET project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		public IActionResult Resume()
+		{
+			return View();
+		}
+		public IActionResult Portfolio()
+		{
+			return View();
+		}
+		public IActionResult PortfolioDetails()
+		{
+			return View();
+		}
+		public IActionResult Services()
+		{
+			return View();
+		}
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+        {
+            return View();
+        }
+
+        
+    }
+}
